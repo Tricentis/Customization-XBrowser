bb3a56fe8ef894a960af675149a9c4dd51452bee
Kavita Gupta <k.gupta@tricentis.com>
2017-07-13 15:13:22 +0530

    Delete Firefox.bat

--- TOSCA Advanced ToolKit Package/Artifacts/Special Execution Tasks/SET_ToolKit/bin/Debug/Firefox.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set DataDir=C:\Users\%USERNAME%\AppData\Local\Mozilla\Firefox\Profiles
-
-del /q /s /f "%DataDir%"
-rd /s /q "%DataDir%"
-
-for /d %%x in (C:\Users\%USERNAME%\AppData\Roaming\Mozilla\Firefox\Profiles\*) do del /q /s /f %%x\*sqlite
